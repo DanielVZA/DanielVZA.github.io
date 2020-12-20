c758fb76147843e4c3b896c3f3295f982f430fdf
DanielVZA <danielvasquez@protonmail.com>
2020-12-20 16:54:02 -0300

    Vista heroes ok

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ var main = new Vue({
   data: {
     url: "https://superheroapi.com/api.php/630240554391029/",
     heroes: [],
+    datos_heroe: {
+      name: '',
+      biography: {},
+      appearance: {},
+      work:{},
+      connections: {}
+    },
   },
   methods: {
     async cargarLista() {
@@ -10,6 +17,9 @@ var main = new Vue({
         var resultado = await axios.get(this.url+i);
         this.heroes.push(resultado.data);
       }
+    },
+    test: function(){
+      alert('ok');
     },
     cargarIndex: function(){
       var index = document.getElementById('index');
@@ -34,7 +44,14 @@ var main = new Vue({
       superHero.style.display = 'none';
       var index = document.getElementById('index');
       index.style.display = 'none'; 
-    }
+    },
+    cargarModalHero: function(heroe){
+      this.datos_heroe.name = heroe.name;
+      this.datos_heroe.biography = heroe.biography;
+      this.datos_heroe.appearance = heroe.appearance;
+      this.datos_heroe.work = heroe.work;
+      this.datos_heroe.connections = heroe.connections;
+    },
   },
   created() {
     this.cargarLista();
